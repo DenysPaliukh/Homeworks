74ea9bcb928281dbaf330d12b25ae2241d13f25b
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-01 23:01:07 +0200

    111

--- Task_3.py
+++ Task_3.py
@@ -2,6 +2,10 @@
 
 import math
 
+value_1 = "Число_1"
+value_2 = "Число_2"
+
+
 try:
     value_1 = float(input("Please enter the first number: "))
 except ValueError:
@@ -10,29 +14,23 @@ except ValueError:
 try:
     value_2 = float(input("Please enter the second number: "))
 except ValueError:
-    print(float(input("It's not a number. Try again: ")))
+    print("It's not a number. Try again.")
 
-try:
-    value_operator = input("Please choose operator:\n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/'\n your answer:")
-except ValueError:
-    print(input("Wrong operator! Try again: "))
+value_operator = input("Please choose operator:\n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/'\n your answer:")
 
-try:
-    if value_operator == '1':
-        result = value_1 + value_2
+if value_operator == '1':
+    result = value_1 + value_2
 
-    elif value_operator == '2':
-        result = value_1 - value_2
+elif value_operator == '2':
+    result = value_1 - value_2
 
-    elif value_operator == '3':
-        result = value_1 * value_2
+elif value_operator == '3':
+    result = value_1 * value_2
 
-    elif value_operator == '4':
-        result = value_1 / value_2
+elif value_operator == '4':
+    result = value_1 / value_2
 
-except ZeroDivisionError:
-    print(input("You can't divide by zero!"))
+    print(result)
 
-print(result)
-
-
+else:
+    print("Wrong operator! Try again.")
