d44b8beac6dbb1ebd05b4de2fadcd81664172f31
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-03 11:05:47 +0200

    Тернарный оператор

---
+++ Task_4.py
@@ -0,0 +1,43 @@
+################################# 1
+
+value = int(input())
+
+new_value = value / 2 if value < 100 else new_value = - value
+
+print(new_value)
+
+################################ 2
+
+value = int(input())
+
+new_value = 1 if value < 100 else new_value = 0
+
+print(new_value)
+
+################################ 3
+
+value = int(input())
+
+new_value = True if value < 100 else new_value = False
+
+print(new_value)
+
+################################ 4
+
+my_str = input()
+
+new_str = 2 * my_str if len(my_str) < 5 else new_str = my_str
+
+print(new_str)
+
+################################ 5
+
+my_str = input()
+
+new_str = my_str + my_str[::-1] if len(my_str) < 5 else new_str = my_str
+
+print(new_str)
+
+
+
+
