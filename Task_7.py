8154f35364065540d0269281bce43fd2b08222c5
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-17 16:30:55 +0200

    Д/з 7

---
+++ Task_7.py
@@ -0,0 +1,90 @@
+# value = input()
+
+# my_list = str(list[value])
+
+# x = my_list.count("0")
+
+# print(x)
+
+##################################
+
+#value = input()
+
+#value_1 = str(value)
+
+#new_list = list(value_1)
+
+#list_2 = []
+
+#result = 0
+
+#for i in new_list:
+
+#        if i == "0" in reversed(new_list):
+#            result += 1
+
+#print(result)
+#print(new_list)
+
+####################################
+
+#my_list_1 = [1, 2, 3, 4, 5]
+
+#my_list_2 = ['A', 'B', 'C', 'D', 'E', 'F']
+
+#my_result = my_list_1[1::2]
+
+#for i in my_list_2[1::2]:
+#    my_result.append(i)
+
+#print(my_result)
+
+#######################################
+
+#my_list = [1, 2, 3, 4]
+
+#new_list = my_list[1:]
+
+#new_list.append(my_list[0])
+
+#print(new_list)
+
+#####################################
+
+#my_list = [1, 2, 3, 4]
+
+#new_list = str(my_list[0])
+
+#my_list.pop(0)
+
+#print(id(my_list))
+
+#my_list.extend(new_list)
+
+#print(id(my_list))
+
+#print(my_list)
+
+######################################
+
+#my_list = [1, 2, 3, '11', '22', 33]
+
+#new_list = []
+
+#for i in my_list:
+#    if type(i) is str:
+#        new_list.append(i)
+
+#print(new_list)
+
+#####################################
+# остальные задачи я в принципе не понимаю как сделать.
+
+
+
+
+
+
+
+
+
