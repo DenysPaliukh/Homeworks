31aece48885a6fc96a72805510bd546e41164c5f
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-19 14:06:55 +0200

    Д/з 7 исправлено

--- Task_7.py
+++ Task_7.py
@@ -1,3 +1,7 @@
+import re
+
+################################### 1
+
 # value = input()
 
 # my_list = str(list[value])
@@ -6,85 +10,136 @@
 
 # print(x)
 
-##################################
+################################## 2
 
-#value = input()
+# value = 1005000000
 
-#value_1 = str(value)
+# value_1 = str(value)
 
-#new_list = list(value_1)
+# result = 0
 
-#list_2 = []
+# new_str = value_1[::-1]
 
-#result = 0
+# for i in new_str:
 
-#for i in new_list:
+#    if i == "0":
+#        result += 1
+#    else:
+#        break
 
-#        if i == "0" in reversed(new_list):
-#            result += 1
+# print(result)
 
-#print(result)
-#print(new_list)
+#################################### 3
 
-####################################
+# my_list_1 = [1, 2, 3, 4, 5]
 
-#my_list_1 = [1, 2, 3, 4, 5]
+# my_list_2 = ['A', 'B', 'C', 'D', 'E', 'F']
 
-#my_list_2 = ['A', 'B', 'C', 'D', 'E', 'F']
+# my_result = my_list_1[1::2]
 
-#my_result = my_list_1[1::2]
-
-#for i in my_list_2[1::2]:
+# for i in my_list_2[1::2]:
 #    my_result.append(i)
 
-#print(my_result)
+# print(my_result)
 
-#######################################
+####################################### 4
 
-#my_list = [1, 2, 3, 4]
+# my_list = [1, 2, 3, 4]
 
-#new_list = my_list[1:]
+# new_list = my_list[1:]
 
-#new_list.append(my_list[0])
+# new_list.append(my_list[0])
 
-#print(new_list)
+# print(new_list)
 
-#####################################
+##################################### 5
 
-#my_list = [1, 2, 3, 4]
+# my_list = [1, 2, 3, 4]
 
-#new_list = str(my_list[0])
+# new_list = str(my_list[0])
 
-#my_list.pop(0)
+# my_list.pop(0)
 
-#print(id(my_list))
+# print(id(my_list))
 
-#my_list.extend(new_list)
+# my_list.extend(new_list)
 
-#print(id(my_list))
+# print(id(my_list))
 
-#print(my_list)
+# print(my_list)
 
-######################################
+###################################### 6
+#import re
 
-#my_list = [1, 2, 3, '11', '22', 33]
+#my_str = "43 більше ніж 34, але менше ніж 56"
+
+#pattern = '[0-9]+'
+#my_list = list(re.findall(pattern, my_str))
 
 #new_list = []
-
 #for i in my_list:
+#    new_list.append(int(i))
+
+#result = sum(new_list)
+#print(result)
+
+####################################### 7
+
+# my_list = [2, 4, 1, 5, 3, 9, 0, 7]
+
+# result = 0
+
+# for i in range(1, len(my_list) - 1):
+
+#    if my_list[i] > my_list[i - 1] + my_list[i + 1]:
+#        result += 1
+
+# print(result)
+
+######################################## 8
+
+# my_list = [1, 2, 3, '11', '22', 33]
+
+# new_list = []
+
+# for i in my_list:
 #    if type(i) is str:
+#        new_list.append(i)
+
+# print(new_list)
+
+##################################### 9
+
+#my_str = "uutjghfyrhhhhhhdnbcbbbbb"
+#new_list = []
+
+#for i in my_str:
+#    if my_str.count(i) == 1:
 #        new_list.append(i)
 
 #print(new_list)
 
-#####################################
-# остальные задачи я в принципе не понимаю как сделать.
 
+##################################### 10
 
+#my_str_1 = "aaaasdf1"
+#my_str_2 = "asdfff2"
 
+#new_list = list(set(my_str_1).intersection(set(my_str_2)))
 
+#print(new_list)
 
+###################################### 11
 
+#my_str_1 = "aaaasdf1"
+#my_str_2 = "asdfff2"
 
+#new_list = []
+
+#for i in set(my_str_1).intersection(set(my_str_2)):
+#    if my_str_1.count(i) == 1 and my_str_2.count(i) == 1:
+#        new_list.append(i)
+
+#print(new_list)
 
 
---
+++ Task_8.py
@@ -0,0 +1,51 @@
+
+#my_list = ["one", "two", "three", "four", "five"]
+
+#new_list = []
+
+#for i in range(0, len(my_list)):
+#    if i % 2 != 0:
+#        new_list.append(my_list[i])
+
+#    else:
+#        new_list.append(my_list[i][::-1])
+
+#print(new_list)
+
+#####################################################
+
+#my_list = ["aone", "two", "three", "afour", "five"]
+
+#new_list = []
+
+#for i in range(0, len(my_list)):
+#    if "a" in my_list[i][0:]:
+#        new_list.append(my_list[i])
+
+#print(new_list)
+
+#####################################################
+
+#my_list = ["aone", "twoa", "three", "afour", "fivea"]
+
+#new_list = []
+
+#for i in range(0, len(my_list)):
+#    if "a" in my_list[i]:
+#        new_list.append(my_list[i])
+
+#print(new_list)
+
+######################################################
+
+my_list = [{"name": "John", "age": 15}, {"name": "Jane", "age": 25}, {"name": "Jack", "age": 45}]
+
+name_list = []
+
+len_name_list = []
+
+dict1, dict2, dict3 = my_list
+
+
+
+
