8760a3d5fbf051e5ee2e30723d0b21d2958c6827
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-30 14:04:17 +0300

    Д/з 10 исправлено

--- Task_10.py
+++ Task_10.py
@@ -1,15 +1,12 @@
 import pathlib
 from pathlib import Path
 
-#a = open("authors.txt", "x")
-
 ############################################ 1
-# path = Path("domains.txt")
-# filename = path
+#path = Path("domains.txt")
 
 
-# def domains(kkk):
-#    with open(filename, 'r') as my_file:
+#def domains(path):
+#    with open(path, 'r') as my_file:
 #        data_str = ''.join(my_file.read())
 #        my_str = data_str.replace(".", "")
 #        my_list = my_str.split("\n")
@@ -17,18 +14,17 @@ from pathlib import Path
 #    return my_list
 
 
-# result = domains(path)
-# print(result)
+#result = domains(path)
+#print(result)
 
 ############################################# 2
 
 # path = Path("names.txt")
-# filename = path
 # name_list = []
 
 
-# def names(kkk):
-#    with open(filename, "r") as my_file:
+# def names(path):
+#    with open(path, "r") as my_file:
 #        data_list = my_file.readlines()
 #        for name in data_list:
 #            name_list.append(name.split("\t")[1])
@@ -36,26 +32,21 @@ from pathlib import Path
 #    return name_list
 
 
-# result = names(name_list)
-# print(result)
-
+# print(names(path))
 
 ############################################# 3
 
-path = Path("authors.txt")
-filename = path
-new_list = []
+# path = Path("authors.txt")
+# new_list = []
 
 
-def dict_authors(kkk):
-    with open(filename, "r") as my_file:
-        data_list = my_file.readlines()
-        for data in data_list:
-            if data and "-" in data:
-                new_list.append({"date": data.split(" - ")[0]})
+# def dict_authors(path):
+#    with open(path, "r") as my_file:
+#        data_list = my_file.readlines()
+#        for data in data_list:
+#            if data and "-" in data:
+#                new_list.append({"date": data.split(" - ")[0]})
+#    return new_list
 
-    return new_list
 
-
-result = dict_authors(new_list)
-print(result)
+# print(dict_authors(path))
