34365a4698044a5ff8742eb8f86cc56d7858c797
DenysPaliukh <pdv.dnepr@gmail.com>
2023-03-31 16:24:31 +0300

    Д/з 11

--- Task_11.py
+++ Task_11.py
@@ -1,50 +1,53 @@
 import os
-import pathlib
 ############################################## 1
 
 filename = "../Homeworks"
 dir_list = []
 file_list = []
 dict_files = {}
-file_list_1 = []
-dir_list_1 = []
+
+files_list = os.listdir(filename)
+for file in files_list:
+    if os.path.isdir(file):
+        dir_list.append(file)
+    else:
+        file_list.append(file)
 
 
+def dir_func(files_list):
+    dict_files = {"filenames": file_list, "dirnames": dir_list}
 
-
-def dir_func(filename):
-    files_list = os.listdir(filename)
-    for file in files_list:
-        if os.path.isdir(file):
-            dir_list.append(file)
-        else:
-            file_list.append(file)
-
-    dict_files = {"filenames": file_list_1.append(file_list), "dirnames": dir_list_1.append(dir_list)}
     return dict_files
 
 
-print(dir_func(filename))
+print(dir_func(file_list))
+
 
 ############################################# 2
-#key = False
-#new_dict = {}
+
+def dir_sorting(files_list, key=False):
+    for file in files_list:
+        files_list.sort(reverse=False)
+
+    return files_list
 
 
-#def sorting_dict(dict_files, key):
-#    files_list = os.listdir()
-#    for file in files_list:
-#        if os.path.isdir(file):
-#            dir_list.append(file)
-#        else:
-#            file_list.append(file)
+print(dir_sorting(files_list, key=False))
 
-#        if key is False:
-#            file_list.sort(reverse=True)
-#            dir_list.sort(reverse=True)
-#            dict_files = {"filenames": file_list, "dirnames": dir_list}
+############################################# 3
 
-#    return dict_files
+name = input("введите имя папки/файла: ")
 
 
-#print(sorting_dict(dict_files, key))
+def dict_renew(dict_files, name):
+    if '.' in name:
+        file_list.append(name)
+    else:
+        dir_list.append(name)
+
+    dict_files = {"filenames": file_list, "dirnames": dir_list}
+
+    return dict_files
+
+
+print(dict_renew(dict_files, name))
